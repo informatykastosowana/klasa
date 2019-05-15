7193b9932a7251f1829ec3933cbf383d158b3e3a
jakubtomal <jakub.tomal@smcebi.edu.pl>
2019-05-15 20:28:23 +0200

    klasa pracownik dziedziczaca po klasie osoba

---
+++ Jakub_Tomal/dziedziczenie.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+
+class osoba
+{
+public:
+    osoba(string imie , string nazwisko , long long pesel):pesel(pesel)
+    {
+        this->imie = imie;
+        this->nazwisko = nazwisko;
+    }
+
+    void wypisz()
+    {
+        cout<<imie<<"\n"<<nazwisko<<"\n"<<pesel<<endl;
+    }
+
+    string GetImie(){return imie;}
+    string GetNazwisko(){return nazwisko;}
+
+    long long GetPesel(){return pesel;}
+private:
+    string imie;
+    string nazwisko;
+    const long long pesel;
+};
+
+
+
+class pracownik : public osoba
+{
+public:
+    pracownik(osoba o , int nrLegitymacji):osoba(o.GetImie() ,o.GetNazwisko(),o.GetPesel())
+    {
+        this->nrLegitymacji = nrLegitymacji;
+    }
+
+    int GetNrLegitymacji(){return nrLegitymacji;}
+private:
+    int nrLegitymacji;
+};
+
+
+
+int main()
+{
+    osoba *o1 = new osoba("adam" , "zagajnik" , 96020708255);
+
+    o1->wypisz();
+
+    pracownik *p1 = new pracownik(*o1 , 21);
+
+
+    cout<<"-------------"<<endl;
+    p1->wypisz();
+    cout<<p1->GetNrLegitymacji()<<endl;
+}
