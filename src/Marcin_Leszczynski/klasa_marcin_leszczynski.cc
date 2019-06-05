0025688c3cf58ad7cae5d76b79bb6f4fe5f87388
informatykastosowana <50331609+informatykastosowana@users.noreply.github.com>
2019-06-05 14:56:29 +0200

    Add files via upload

--- src/Marcin_Leszczynski/klasa_marcin_leszczynski.cc
+++ src/Marcin_Leszczynski/klasa_marcin_leszczynski.cc
@@ -1,5 +1,122 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+
+//metody abstrakcyjne
+
+class Pojazdy_latajace{
+
+public:
+    virtual void setTyp(string rodzaj) = 0;
+    virtual string getTyp() =0;
+
+};
+
+class Helikopter : public Pojazdy_latajace
+{
+public:
+
+    Helikopter(string rodzaj1){
+        rodzaj = rodzaj1;
+        typ = "wiroplat";
+        cout<<"Stworzono obiekt : "<<rodzaj<<endl;
+    }
+
+    void setTyp(string rodzaj){
+        typ = rodzaj;
+    }
+
+    string getTyp(){
+        return typ;
+    }
+
+    string typ;
+    string rodzaj;
+};
+
+
+class pojazd{
+
+private:
+
+    string marka;
+    string model;
+    string pojemnosc;
+
+public:
+
+    static int licznikPojazdow;
+    pojazd(){
+        licznikPojazdow++;
+        cout<<"Stworzono obiekt pojazd"<<endl;
+    };
+
+    ~pojazd(){
+    cout<<"Zniszczono obiekt pojazd"<<endl;
+    }
+
+    void ustawMarka(string marka1){
+        marka = marka1;
+    }
+    void ustawModel(string model1){
+        model = model1;
+    }
+    void ustawPojemnosc(string pojemnosc1){
+        pojemnosc = pojemnosc1;
+    }
+
+    string pobierzMarka(){
+        return marka;
+    }
+    string pobierzModel(){
+        return model;
+    }
+    string pobierzPojemnosc(){
+        return pojemnosc;
+    }
+
+};
+
+int pojazd::licznikPojazdow =0;
+
+class samochod : public pojazd{
+
+private:
+    int liczbaKola;
+    int przebieg;
+
+public:
+    samochod(){
+        licznikPojazdow++;
+        cout<<"Stworzono obiekt samochod"<<endl;
+    }
+
+    samochod(string marka, string model){
+
+        ustawMarka(marka);
+        ustawModel(model);
+        licznikPojazdow++;
+        cout<<pobierzMarka()<<", "<<pobierzModel()<<endl;
+    }
+
+    void liczbaKol(int kola){
+        liczbaKola = kola;
+    }
+};
+
+class tir : samochod{
+
+public:
+
+    tir(string marka, string model){
+        licznikPojazdow++;
+        ustawMarka(marka);
+        ustawModel(model);
+    }
+
+};
 
 class klasa{
 
@@ -17,6 +134,13 @@ class klasa{
         zmienna2 = y;
         cout<<"Konstruktor klasy z argumentami\n";
     }
+
+    klasa(const klasa &obiekt){
+        zmienna1 = obiekt.zmienna1;
+        zmienna2 = obiekt.zmienna2;
+        cout<<"Konstruktor kopiujacy"<<endl;
+    }
+
 
     ~klasa(){
     cout<<"Destrukcja zakonczona powodzeniem\n";
@@ -41,7 +165,7 @@ void klasa::metoda2(){cout<<"Metoda zadeklarowana poza cialem klasy\n";}
 
 int main()
 {
-
+/*
    //Pierwszy sposob tworzenia obiektow klasy--------------------------------------
     klasa *obiekt_konstruktor = new klasa;
     obiekt_konstruktor -> metoda1();
@@ -64,6 +188,20 @@ int main()
     obiekt2.setZmienna(2);
     int y =obiekt2.getZmienna();
     cout<<y<<endl;
+
+*/
+
+    //------------klasy dziedziczenie--------------
+    pojazd fiat;
+
+    fiat.ustawMarka("Fiat");
+    fiat.ustawModel("Albea");
+    cout<<fiat.pobierzMarka()<<endl;
+
+    samochod audi("Audi","A4");
+    cout<<"static licznik: "<<audi.licznikPojazdow<<endl;
+
+
 
 return 0;
 }
